221f4e52086d8e5bba47c69261e23e609944025f
tommustbe12com <155601372+tommustbe12com@users.noreply.github.com>
2025-11-28 22:18:30 -0800

    Initial commit

---
+++ src/main/java/com/tommustbe12/simpleHomes/SimpleHomes.java
@@ -0,0 +1,17 @@
+package com.tommustbe12.simpleHomes;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SimpleHomes extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
